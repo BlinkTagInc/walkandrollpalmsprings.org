f437f3226ae243af05c6ce669f8e79e51058405c
Brendan Nee <brendan@blinktag.com>
2015-08-11 14:14:03 -0700

    Mode menu

--- public/jsx/results.jsx
+++ public/jsx/results.jsx
@@ -3,17 +3,20 @@ var classNames = require('classnames');
 var Router = require('react-router');
 var { Link } = Router;
 var map = require('../js/map.js');
+var _ = require('underscore');
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
       windowWidth: window.innerWidth,
       windowHeight: window.innerHeight,
-      mapHeight: 400
+      mapHeight: 400,
+      menuOpen: false,
+      selectedMode: this.props.query.mode
     };
   },
 
-  handleResize: function(e) {
+  handleResize: function() {
     this.setState({
       windowWidth: window.innerWidth,
       windowHeight: window.innerHeight,
@@ -21,11 +24,42 @@ module.exports = React.createClass({
     });
   },
 
+  toggleModeMenu: function() {
+    this.setState({menuOpen: true});
+  },
+
+  selectMode: function(mode) {
+    this.setState({
+      selectedMode: mode,
+      menuOpen: false
+    });
+  },
+
+  renderModeMenu: function() {
+    var menuItems;
+    var modes = ['walk', 'bike', 'transit'];
+
+    if (this.state.menuOpen) {
+      menuItems = _.without(modes, this.state.selectedMode).map(function(mode, idx) {
+        return (
+          <div className={classNames('mode', mode)} key={idx} onClick={this.selectMode.bind(null, mode)}></div>
+        );
+      }.bind(this));
+    }
+
+    return (
+      <div className={classNames('mode-menu', {open: this.state.menuOpen})}>
+        <div className={classNames('mode', 'selected', this.state.selectedMode)} onClick={this.toggleModeMenu}></div>
+        {menuItems}
+      </div>
+    );
+  },
+
   render: function() {
     return (
       <div>
         <div className="section-header section-teal" ref="sectionHeader">
-          <div className={classNames('selected-mode', this.props.query.mode)}></div>
+          {this.renderModeMenu()}
           <h2 className="results-title">Showing:</h2>
           <div className="selected-places">{this.props.query.places.join(', ')}</div>
         </div>
