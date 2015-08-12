d5bc1d4a76bf3483f8a662552e30f1dbb2239c7f
Brendan Nee <brendan@blinktag.com>
2015-08-12 02:19:49 -0700

    Hompage styles

--- public/jsx/welcome.jsx
+++ public/jsx/welcome.jsx
@@ -13,10 +13,12 @@ module.exports = React.createClass({
         <h1 className="welcome-title">Walk and Roll:<br/> Palm Springs</h1>
         <div className="welcome-description">Eat, Play, Shop,<br/>Connect, and Move.<br />By Foot, Bike, and Transit.<br />Save Carbon. Count Calories.</div>
         <Link to="search" className="get-started">Get Started</Link>
-        <div className="welcome-links">
+        <div className="navigation">
+          <Link to="home" className="selected">Home</Link>
+          <Link to="search">Search</Link>
           <Link to="neighborhoods">Neighborhoods</Link>
           <Link to="leaderboard">Leaderboard</Link>
-          <Link to="about">About This Site</Link>
+          <Link to="about">About</Link>
         </div>
       </div>
     );
