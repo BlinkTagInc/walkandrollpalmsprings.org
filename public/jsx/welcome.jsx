a8c8bd90bb02cb2f2448e1c4334da2e4e204a512
Brendan Nee <brendan@blinktag.com>
2015-08-25 05:08:41 -0500

    Button styles

--- public/jsx/welcome.jsx
+++ public/jsx/welcome.jsx
@@ -8,7 +8,7 @@ module.exports = React.createClass({
       <div className="content">
         <h1 className="welcome-title">Walk and Roll:<br/> Palm Springs</h1>
         <div className="welcome-description">Eat, Play, Shop,<br/>Connect, and Move.<br />By Foot, Bike, and Transit.<br />Save Carbon. Count Calories.</div>
-        <Link to="search" className="get-started">Get Started</Link>
+        <Link to="search" className="btn btn-green btn-get-started">Get Started</Link>
         <SiteMenu selected="home" color="red" />
       </div>
     );
