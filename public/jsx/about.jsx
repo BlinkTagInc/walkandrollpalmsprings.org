428131030f4322365652901c5bb8d58febd83937
Brendan Nee <brendan@blinktag.com>
2015-11-13 02:04:41 -0800

    City of palm springs logo

--- public/jsx/about.jsx
+++ public/jsx/about.jsx
@@ -21,6 +21,7 @@ module.exports = class About extends React.Component {
         <div className="section-content">
           <p>This site was created by <a href="http://blinktag.com">BlinkTag, Inc</a>. for the <a href="http://www.ci.palm-springs.ca.us/">City of Palm Springs</a> in 2015. For more information, see: <a href="http://www.healthyplanethealthyyoups.com">Healthy Planet Healthy you</a>.</p>
           <p>Questions? Contact: <a href="mailto:Michele.Mician@palmsprings-ca.gov">Michele.Mician@palmsprings-ca.gov</a></p>
+          <img src="/images/city-of-palm-springs-logo@2x.png" className="about-logo" />
         </div>
         <SiteMenu selected="about" color="green" />
       </div>
--- public/jsx/welcome.jsx
+++ public/jsx/welcome.jsx
@@ -6,13 +6,16 @@ module.exports = class Welcome extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     return (
       <div className="content background-tan">
         <h1 className="welcome-title">Walk and Roll:<br/> Palm Springs</h1>
-        <div className="welcome-description">Eat, Play, Shop,<br/>Connect, and Move.<br />By Foot, Bike, and Transit.<br />Save Carbon. Count Calories.</div>
-        <Link to="search" className="btn btn-green btn-get-started">Get Started</Link>
+        <div className="welcome-description">
+          <div className="welcome-logo"></div>
+          Eat, Play, Shop,<br/>Connect, and Move.<br />By Foot, Bike, and Transit.<br />Save Carbon. Count Calories.
+          <Link to="search" className="btn btn-green btn-get-started">Get Started</Link>
+        </div>
         <SiteMenu selected="home" color="red" />
       </div>
     );
